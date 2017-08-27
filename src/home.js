715a257073b19391a04ea0c8ade633b519d9d2c4
kinjoyuji <kinjoyuji@gmail.com>
2017-08-27 16:53:14 +0900

    export をdefault指定

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class Home extends React.Component {
+export default class Home extends React.Component {
   render() {
     return (
       <div>home</div>
