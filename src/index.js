92bc56ba461f19dd490d4f4e3ee1ef9776b49758
kinjoyuji <kinjoyuji@gmail.com>
2017-08-27 17:04:22 +0900

    rootの修正

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './home';
+import App from './app';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
