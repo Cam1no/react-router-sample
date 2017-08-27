1a12760349941e512c381fc9026da951c8769d8d
kinjoyuji <kinjoyuji@gmail.com>
2017-08-27 16:49:32 +0900

    rootをhomeコンポーネントを呼び出すように

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './home';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>HelloWorld</h1>, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
