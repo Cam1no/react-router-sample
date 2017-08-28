555c02ddeb8615c4a844c7de92c6613492c6d109
kinjoyuji <kinjoyuji@gmail.com>
2017-08-28 17:41:31 +0900

    routesの設定

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import Contact from './contact';
+import About from './about';
+import Home from './home';
 import './index.css';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <BrowserRouter>
+    <div>
+      <App />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </BrowserRouter>
+  ), document.getElementById('root'));
 registerServiceWorker();
